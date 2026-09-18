c5400d59081ab23f3ce8bb1239f07a848c368bf8
Nitin M <nitsm404@gmail.com>
2026-09-18 18:56:09 +0530

    Updates?
    Updates?

--- Beds.c
+++ Beds.c
@@ -1,5 +1,16 @@
+/*Bed Management Menu*/
 #include<stdio.h>
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int menu() 
+{
+    int choice;
+    printf("\n\n===Beds Management Menu===\n");
+    printf("1. Add Beds\n");
+    printf("2. View Beds\n");
+    printf("3. Update Beds\n");
+    printf("4. Delete Beds\n");
+    printf("5. Exit\n");  
+    
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    return choice;
 }
