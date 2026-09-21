d380dee925047599f73dccbaff444bf0b41ce341
hridyabimal <hridyabimal@gmail.com>
2026-09-21 17:27:25 +0530

    Update equipment.c

--- equipment.c
+++ equipment.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+struct Equipment {
+    char item[50];
+    int id;
+    int quantity;
+    char expiry_date[20];
+};
+int main() {
+    struct Equipment e;
+    int choice;
+    do{
+        printf("\n=================== Equipment Management System ===================\n");
+        printf("1. Add Equipment\n");
+        printf("2. Display Equipment\n");
+        printf("3. Update Equipment\n");
+        printf("4. Delete Equipment\n");
+        printf("5. Exit\n");
+        printf("====================================================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            // Add Equipment code
+            break;
+        
+        default:
+            break;
+        }
+    }while(choice!=5);
+}
 
