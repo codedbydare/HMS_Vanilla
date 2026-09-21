cd3128ba549020d7cb34c6165fdfa8377b898d94
hridyabimal <hridyabimal@gmail.com>
2026-09-21 19:32:42 +0530

    Update equipment.c

--- equipment.c
+++ equipment.c
@@ -6,7 +6,7 @@ struct Equipment {
     char expiry_date[20];
 };
 int main() {
-    struct Equipment e;
+    struct Equipment e[50];
     int choice,n;
     do{
         printf("\n=================== Equipment Management System ===================\n");
@@ -35,12 +35,52 @@ int main() {
             }
             break;
         case 2:
-            printf("%s\t%s\t%s\t%s\n", "Item Name", "Item ID", "Quantity", "Expiry Date");
-            printf("%s\t%d\t%d\t%s\n", e.item, e.id, e.quantity, e.expiry_date);
+           if(n == 0) {
+                printf("No equipment items to display.\n");
+            } else {
+                printf("\n=================== Equipment List ===================\n");
+                printf("%-20s %-10s %-10s %-15s\n", "Item Name", "Item ID", "Quantity", "Expiry Date");
+                printf("--------------------------------------------------------\n");
+                for(int i = 0; i < n; i++) {
+                    printf("%-20s %-10d %-10d %-15s\n", e[i].item, e[i].id, e[i].quantity, e[i].expiry_date);
+                }
+            }
             break;
         case 3:
-        
+            printf("Enter the item ID to update: ");
+            int update_id;
+            scanf("%d", &update_id);
+            for(int i = 0; i < n; i++) {
+                if(e[i].id == update_id) {
+                    printf("Enter new item name: ");
+                    scanf("%s", e[i].item);
+                    printf("Enter new quantity: ");
+                    scanf("%d", &e[i].quantity);
+                    printf("Enter new expiry date (YYYY-MM-DD): ");
+                    scanf("%s", e[i].expiry_date);
+                    break;
+                }
+            }
+            break;
+        case 4:
+            printf("Enter the item ID to delete: ");
+            int delete_id;
+            scanf("%d", &delete_id);
+            for(int i = 0; i < n; i++) {
+                if(e[i].id == delete_id) {
+                    for(int j = i; j < n - 1; j++) {
+                        e[j] = e[j + 1];
+                    }
+                    n--;
+                    break;
+                }
+            }
+            break;
+        case 5:
+            printf("Exiting the program.\n");
+            break;
         default:
+            printf("Invalid choice. Please try again.\n");
             break;
         }
     }while(choice!=5);
