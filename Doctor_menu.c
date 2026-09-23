c79220ab7f6d9881eb4a5960e69d16d0e41bad5c
Shruti-2620 <shrutithapa4321@gmail.com>
2026-09-24 02:27:58 +0530

    Update Doctor_menu.c
    ADDED  structure

--- Doctor_menu.c
+++ Doctor_menu.c
@@ -1,9 +1,50 @@
 /*Doctor_Menu*/
 #include <stdio.h>
 void addDoctor(){
+struct Doctor{
+    int id;
+    char name[50];
+    char specialization[50];
+    char hospital[100];
+    char phone[15];
+}doctor[100];
+void addDoctor(int doccount){
+    printf("Enter Doctor ID: ");
+    scanf("%d", &doctor[0].id);
+
+    printf("Enter Doctor Name: ");
+    scanf("%s", doctor[0].name);
+
+    printf("Enter Doctor Specialization: ");
+    scanf("%s", doctor[0].specialization);
+
+    printf("Enter Doctor Phone: ");
+    scanf("%s", doctor[0].phone);
+
+    printf("Enter Doctor Hospital: ");
+    scanf("%s", doctor[0].hospital);
+
+    doctor[doccount] = doctor[0];
+    doccount++;
+    printf("Doctor added successfully!\n");
 
 }
+void viewDoctor(int doccount){
+    if(doccount == 0){
+        printf("No doctors available.\n");
+        return;
+    }
+    printf("Doctor List:\n");
+    for(int i = 0; i < doccount; i++){
+         printf(" ID  name  Specialization  Phone  Hospital\n");
+        printf("%d  %s  %s  %s  %s\n", doctor[i].id, doctor[i].name, doctor[i].specialization, doctor[i].phone, doctor[i].hospital);
+    }
+}
+void update(int doccount){
+    
+}
 int main(){
+    int doccount = 0;
     printf("----Docter MENU----\n");
     int choice;
     printf("Menu:\n");
